1cd022359f9af1528c93a42a7aca0c8e169ad5ed
Technifutur <600@technifutur.be>
2017-12-13 12:09:05 +0100

    Test On va tout craché

---
+++ Assets/Katamari Oli Jer/Team/Oli/Scripts/MovePlayer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovePlayer : MonoBehaviour
+
+
+    
+{
+
+    #region Public Members
+    public float moveSpeed = 0.3f;
+    public float rotationSpeed = 8f;
+
+    public Transform m_transform;
+    public float m_movementSpeed;
+
+    #endregion
+
+    #region Public Void
+
+    #endregion
+
+    #region System
+    private void Move()
+    {
+        /*
+        m_previousPosition = m_transform.position;
+        m_previousposition.x += imput.Getaxis("Horizontal");
+        m_transform.position = m_transform previousPosition;
+        */
+    }
+
+    private void Awake()
+    {
+       
+    }
+    void Start () 
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+
+        {
+
+            transform.Translate(Vector3.forward * moveSpeed);
+
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+
+        {
+
+            transform.Translate(-Vector3.forward * moveSpeed);
+
+        }
+    }
+	
+	void Update () 
+    {
+      
+	}
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+   
+    #endregion
+}
