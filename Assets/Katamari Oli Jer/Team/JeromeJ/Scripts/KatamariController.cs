4cc90d6d6064f0e4f4d0326a05faf8549f6bdeb3
JeromeJ <jerome.jamart.42@gmail.com>
2017-12-13 12:41:21 +0100

    Katamari now sticks to collectibles

---
+++ Assets/Katamari Oli Jer/Team/JeromeJ/Scripts/KatamariController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatamariController : MonoBehaviour
+{
+    #region Public Members
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    void Start ()
+    {
+        
+    }
+
+    void Update ()
+    {
+        	
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "collectible")
+            collision.transform.parent = this.transform;
+    }
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
